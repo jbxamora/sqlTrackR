cf3a53350b774bc792bdac37ef78bce76408c751
jbxamora <jbxamora@icloud.com>
2023-02-26 16:02:11 -0800

    remade original use case structure. will be doing one function at a time then testing before moving onto the next.

--- db/queries.js
+++ db/queries.js
@@ -1,176 +1,176 @@
-const connection = require('../config/connection.js');
-const mysql = require('mysql2/promise');
-// Query to return all departments
+// const connection = require('../config/connection.js');
+// const mysql = require('mysql2/promise');
+// // Query to return all departments
 
-const findAllDepartments = () => {
-    return connection.promise().query("SELECT * FROM department")
-        .then(([rows]) => {
-            return rows;
-        });
-};
+// const findAllDepartments = () => {
+//     return connection.promise().query("SELECT * FROM department")
+//         .then(([rows]) => {
+//             return rows;
+//         });
+// };
 
-// Query to add a new department 
-function createDepartment(department) {
-    return connection.query(
-        'INSERT INTO department SET?',
-        department
-    );
-}
+// // Query to add a new department 
+// function createDepartment(department) {
+//     return connection.query(
+//         'INSERT INTO department SET?',
+//         department
+//     );
+// }
 
-// Query to return all roles
+// // Query to return all roles
 
-function findAllRoles() {
-    return connection.query(
-        'SELECT role.id, role.title, department.name AS department, role.salary FROM role LEFT JOIN department ON role.department_id = department.id ORDER BY role.id'
-    );
-}
-
-
-// Query to add a new role 
-function createRole(role) {
-    return connection.query(
-        'INSERT INTO role SET?',
-        role
-    );
-}
-
-// Query to return all employees
-
-function findAllEmployees() {
-    return connection.query(
-        `SELECT employee.id, employee.first_name, employee.last_name, role.title AS role, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager
-        FROM employee 
-        LEFT JOIN role ON employee.role_id = role.id 
-        LEFT JOIN department ON role.department_id = department.id 
-        LEFT JOIN employee manager ON employee.manager_id = manager.id 
-        ORDER BY employee.id`
-    );
-}
+// function findAllRoles() {
+//     return connection.query(
+//         'SELECT role.id, role.title, department.name AS department, role.salary FROM role LEFT JOIN department ON role.department_id = department.id ORDER BY role.id'
+//     );
+// }
 
 
-// Query to add a new employee
+// // Query to add a new role 
+// function createRole(role) {
+//     return connection.query(
+//         'INSERT INTO role SET?',
+//         role
+//     );
+// }
 
-function createEmployee(employee) {
-    return connection.query(
-        'INSERT INTO employee SET?',
-        employee
-    );
-}
+// // Query to return all employees
 
-// Remove an employee
-function removeEmployee(employeeId) {
-    return connection.query(
-        "DELETE FROM employee WHERE id = ?",
-        employeeId
-    );
-}
-
-// Query to update employee role
-
-function updateEmployeeRole(employeeId, roleId) {
-    return connection.query(
-        `UPDATE employee SET role_id =? WHERE id =?`,
-        [roleId, employeeId]
-    );
-}
-
-// Add a role
-function addRole(role) {
-    return connection.query("INSERT INTO role SET ?", role);
-}
-
-// Remove a role
-function removeRole(roleId) {
-    return connection.query("DELETE FROM role WHERE id = ?", roleId);
-}
-
-// View all roles
-function viewAllRoles() {
-    return connection.query(
-        "SELECT role.id, role.title, department.name AS department, role.salary FROM role LEFT JOIN department on role.department_id = department.id;"
-    );
-}
-
-// Query to update employee manager
-
-function updateEmployeeManager(employeeId, managerId) {
-    return connection.query(
-        `UPDATE employee SET manager_id =? WHERE id =?`,
-        [managerId, employeeId]
-    );
-}
+// function findAllEmployees() {
+//     return connection.query(
+//         `SELECT employee.id, employee.first_name, employee.last_name, role.title AS role, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+//         FROM employee 
+//         LEFT JOIN role ON employee.role_id = role.id 
+//         LEFT JOIN department ON role.department_id = department.id 
+//         LEFT JOIN employee manager ON employee.manager_id = manager.id 
+//         ORDER BY employee.id`
+//     );
+// }
 
 
-// Query to return all employees by department
+// // Query to add a new employee
 
-    function findAllEmployeesByDepartment(departmentName) {
-        return connection.promise().query(
-            `SELECT employee.id, employee.first_name, employee.last_name, role.title AS role, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager 
-        FROM employee 
-        LEFT JOIN role ON employee.role_id = role.id 
-        LEFT JOIN department ON role.department_id = department.id 
-        LEFT JOIN employee manager ON employee.manager_id = manager.id 
-        WHERE department.name = ?
-        ORDER BY employee.id`,
-            [departmentName]
-        );
-    }
+// function createEmployee(employee) {
+//     return connection.query(
+//         'INSERT INTO employee SET?',
+//         employee
+//     );
+// }
+
+// // Remove an employee
+// function removeEmployee(employeeId) {
+//     return connection.query(
+//         "DELETE FROM employee WHERE id = ?",
+//         employeeId
+//     );
+// }
+
+// // Query to update employee role
+
+// function updateEmployeeRole(employeeId, roleId) {
+//     return connection.query(
+//         `UPDATE employee SET role_id =? WHERE id =?`,
+//         [roleId, employeeId]
+//     );
+// }
+
+// // Add a role
+// function addRole(role) {
+//     return connection.query("INSERT INTO role SET ?", role);
+// }
+
+// // Remove a role
+// function removeRole(roleId) {
+//     return connection.query("DELETE FROM role WHERE id = ?", roleId);
+// }
+
+// // View all roles
+// function viewAllRoles() {
+//     return connection.query(
+//         "SELECT role.id, role.title, department.name AS department, role.salary FROM role LEFT JOIN department on role.department_id = department.id;"
+//     );
+// }
+
+// // Query to update employee manager
+
+// function updateEmployeeManager(employeeId, managerId) {
+//     return connection.query(
+//         `UPDATE employee SET manager_id =? WHERE id =?`,
+//         [managerId, employeeId]
+//     );
+// }
+
+
+// // Query to return all employees by department
+
+//     function findAllEmployeesByDepartment(departmentName) {
+//         return connection.promise().query(
+//             `SELECT employee.id, employee.first_name, employee.last_name, role.title AS role, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager 
+//         FROM employee 
+//         LEFT JOIN role ON employee.role_id = role.id 
+//         LEFT JOIN department ON role.department_id = department.id 
+//         LEFT JOIN employee manager ON employee.manager_id = manager.id 
+//         WHERE department.name = ?
+//         ORDER BY employee.id`,
+//             [departmentName]
+//         );
+//     }
 
 
 
-// Query to return all employees by manager
+// // Query to return all employees by manager
 
-function findAllEmployeesByManager(managerId) {
-    return connection.promise().query(
-        `SELECT employee.id, employee.first_name, employee.last_name, role.title AS role, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager 
-        FROM employee 
-        LEFT JOIN role ON employee.role_id = role.id 
-        LEFT JOIN department ON role.department_id = department.id 
-        LEFT JOIN employee manager ON employee.manager_id = manager.id 
-        WHERE employee.manager_id = ?
-        ORDER BY employee.id`,
-        [managerId]
-    );
-}
+// function findAllEmployeesByManager(managerId) {
+//     return connection.promise().query(
+//         `SELECT employee.id, employee.first_name, employee.last_name, role.title AS role, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager 
+//         FROM employee 
+//         LEFT JOIN role ON employee.role_id = role.id 
+//         LEFT JOIN department ON role.department_id = department.id 
+//         LEFT JOIN employee manager ON employee.manager_id = manager.id 
+//         WHERE employee.manager_id = ?
+//         ORDER BY employee.id`,
+//         [managerId]
+//     );
+// }
 
-// Query to delete a department
+// // Query to delete a department
 
-function deleteDepartment(departmentId) {
-    return connection.query(
-        `DELETE FROM department WHERE id =?`,
-        departmentId
-    );
-}
+// function deleteDepartment(departmentId) {
+//     return connection.query(
+//         `DELETE FROM department WHERE id =?`,
+//         departmentId
+//     );
+// }
 
-// Add a department
-function addDepartment(department) {
-    return connection.query("INSERT INTO department SET ?", department);
-}
+// // Add a department
+// function addDepartment(department) {
+//     return connection.query("INSERT INTO department SET ?", department);
+// }
 
-// View the total utilized budget of a department
-function viewDepartmentBudget(departmentId) {
-    return connection.query(
-        "SELECT SUM(salary) AS utilized_budget FROM employee LEFT JOIN role on employee.role_id = role.id where role.department_id = ?;",
-        departmentId
-    );
-}
+// // View the total utilized budget of a department
+// function viewDepartmentBudget(departmentId) {
+//     return connection.query(
+//         "SELECT SUM(salary) AS utilized_budget FROM employee LEFT JOIN role on employee.role_id = role.id where role.department_id = ?;",
+//         departmentId
+//     );
+// }
 
-module.exports = {
-    findAllEmployees,
-    findAllDepartments,
-    findAllEmployeesByDepartment,
-    findAllEmployeesByManager,
-    findAllRoles,
-    createDepartment,
-    createEmployee,
-    createRole,
-    deleteDepartment,
-    updateEmployeeManager,
-    updateEmployeeRole,
-    addDepartment,
-    addRole,
-    removeEmployee,
-    removeRole,
-    viewAllRoles,
-    viewDepartmentBudget,
-}
+// module.exports = {
+//     findAllEmployees,
+//     findAllDepartments,
+//     findAllEmployeesByDepartment,
+//     findAllEmployeesByManager,
+//     findAllRoles,
+//     createDepartment,
+//     createEmployee,
+//     createRole,
+//     deleteDepartment,
+//     updateEmployeeManager,
+//     updateEmployeeRole,
+//     addDepartment,
+//     addRole,
+//     removeEmployee,
+//     removeRole,
+//     viewAllRoles,
+//     viewDepartmentBudget,
+// }
--- index.js
+++ index.js
@@ -1,19 +1,106 @@
-// // test connection
-// const mysql = require('mysql2');
-// const connection = require('./config/connection');
-// // // create the connection to database
-// // const connection = mysql.createConnection({
-// //     host: 'localhost',
-// //     user: 'root',
-// //     database: 'test'
-// // });
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+require("console.table");
 
-// // test query -works
-// connection.query(
-//     'SELECT * FROM employee;',
-//     function (err, results, fields) {
-//         console.log(results); // results contains rows returned by server
-//         console.log(err); // fields contains extra meta data about results, if available
-//     }
-// );
+
+// Create the connection to database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'FakePassword',
+    database: 'company_db'
+});
+connection.connect(function (err) {
+    if (err) throw err;
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    start();
+});
+
+function start() {
+    inquirer
+        .prompt({
+            name: "action",
+            type: "list",
+            message: "What would you like to do?",
+            choices: [
+                "View all employees",
+                "View all employees by department",
+                "View all employees by manager",
+                "Add employee",
+                "Remove employee",
+                "Update employee role",
+                "Update employee manager",
+                "View all roles",
+                "Add role",
+                "Remove role",
+                "View all departments",
+                "Add department",
+                "Remove department",
+                "Quit"
+            ]
+        })
+        .then(function (answer) {
+            switch (answer.action) {
+                case "View all employees":
+                    viewAllEmployees();
+                    break;
+
+                case "View all employees by department":
+                    viewAllEmployeesByDepartment();
+                    break;
+
+                case "View all employees by manager":
+                    viewAllEmployeesByManager();
+                    break;
+
+                case "Add employee":
+                    addEmployee();
+                    break;
+
+                case "Remove employee":
+                    removeEmployee();
+                    break;
+
+                case "Update employee role":
+                    updateEmployeeRole();
+                    break;
+
+                case "Update employee manager":
+                    updateEmployeeManager();
+                    break;
+
+                case "View all roles":
+                    viewAllRoles();
+                    break;
+
+                case "Add role":
+                    addRole();
+                    break;
+
+                case "Remove role":
+                    removeRole();
+                    break;
+
+                case "View all departments":
+                    viewAllDepartments();
+                    break;
+
+                case "Add department":
+                    addDepartment();
+                    break;
+
+                case "Remove department":
+                    removeDepartment();
+                    break;
+
+                case "Quit":
+                    connection.end();
+                    break;
+            }
+        });
+}
 
